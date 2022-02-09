2e0fce3900b393d87736e8bf5b41addc4d51e208
DongHee-Sin <voslr0601@naver.com>
2022-02-09 20:00:04 +0900

    광고 아래에 있는 카테고리들 수정 완료

--- Autolayout/ViewController.swift
+++ Autolayout/ViewController.swift
@@ -11,14 +11,12 @@ class ViewController: UIViewController, UIScrollViewDelegate {
 
     @IBOutlet weak var searchButton: UIButton!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         // 메인화면의 검색버튼 테두리 지정
         searchButton.layer.borderWidth = 1
         searchButton.layer.borderColor = UIColor(red:222/255, green:225/255, blue:227/255, alpha: 1).cgColor
         searchButton.layer.cornerRadius = 7
-
     }
 }
