cce9e5fed6a0c66f4ca8a6d51954938f71e0c20c
DongHee-Sin <voslr0601@naver.com>
2022-02-09 15:03:25 +0900

    수정사항 1. 페이지 전체를 스크롤할 수 있는 ScrollView 추가 2. 코드로 작성했던 ScrollView(광고 이미지)를 AutoLayout을 사용하여 구성

--- Autolayout/ViewController.swift
+++ Autolayout/ViewController.swift
@@ -11,37 +11,37 @@ class ViewController: UIViewController, UIScrollViewDelegate {
 
     @IBOutlet weak var searchButton: UIButton!
     
-    @IBOutlet weak var commercialScrollView: UIScrollView!
-    @IBOutlet weak var commercialPageControl: UIPageControl!
+//    @IBOutlet weak var commercialScrollView: UIScrollView!
+//    @IBOutlet weak var commercialPageControl: UIPageControl!
     
-    // 메인 스크롤 광고
-    var commercialImages: [String] = ["광고1", "광고2", "광고3", "광고4", "광고5"]
-    var commercialImageViews = [UIImageView]()
-    
-    
-    private func addContentScrollView() {
-        for i in 0..<commercialImages.count {
-            let imageView = UIImageView()
-            let xPos = self.view.frame.width * CGFloat(i)
-            imageView.frame = CGRect(x: xPos, y: 0, width: commercialScrollView.bounds.width, height: commercialScrollView.bounds.height)
-            imageView.image = UIImage(named: commercialImages[i])
-            commercialScrollView.addSubview(imageView)
-            commercialScrollView.contentSize.width = imageView.frame.width * CGFloat(i + 1)
-        }
-    }
-    
-    private func setPageControll() {
-        commercialPageControl.numberOfPages = commercialImages.count
-    }
-    
-    private func setPageControlSelectedPage(currentPage: Int) {
-        commercialPageControl.currentPage = currentPage
-    }
-    
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        let value: CGFloat = scrollView.contentOffset.x / scrollView.frame.size.width
-        setPageControlSelectedPage(currentPage: Int(round(value)))
-    }
+//    // 메인 스크롤 광고
+//    var commercialImages: [String] = ["광고1", "광고2", "광고3", "광고4", "광고5"]
+//    var commercialImageViews = [UIImageView]()
+//
+//
+//    private func addContentScrollView() {
+//        for i in 0..<commercialImages.count {
+//            let imageView = UIImageView()
+//            let xPos = self.view.frame.width * CGFloat(i)
+//            imageView.frame = CGRect(x: xPos, y: 0, width: commercialScrollView.bounds.width, height: commercialScrollView.bounds.height)
+//            imageView.image = UIImage(named: commercialImages[i])
+//            commercialScrollView.addSubview(imageView)
+//            commercialScrollView.contentSize.width = imageView.frame.width * CGFloat(i + 1)
+//        }
+//    }
+//
+//    private func setPageControll() {
+//        commercialPageControl.numberOfPages = commercialImages.count
+//    }
+//
+//    private func setPageControlSelectedPage(currentPage: Int) {
+//        commercialPageControl.currentPage = currentPage
+//    }
+//
+//    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+//        let value: CGFloat = scrollView.contentOffset.x / scrollView.frame.size.width
+//        setPageControlSelectedPage(currentPage: Int(round(value)))
+//    }
     
     
     override func viewDidLoad() {
@@ -52,9 +52,9 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         searchButton.layer.borderColor = UIColor(red:222/255, green:225/255, blue:227/255, alpha: 1).cgColor
         searchButton.layer.cornerRadius = 7
         
-        // 메인 스크롤 광고
-        commercialScrollView.delegate = self
-        addContentScrollView()
-        setPageControll()
+//        // 메인 스크롤 광고
+//        commercialScrollView.delegate = self
+//        addContentScrollView()
+//        setPageControll()
     }
 }
