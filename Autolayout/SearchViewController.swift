4ce0dabfe4a255fa4278a5d7eb969166b5c9d0af
DongHee-Sin <voslr0601@naver.com>
2022-02-11 16:00:48 +0900

    추천검색어 작업완료

--- Autolayout/SearchViewController.swift
+++ Autolayout/SearchViewController.swift
@@ -15,7 +15,8 @@ class SearchViewController: UIViewController {
     @IBOutlet weak var recentSearchStackView: UIStackView!
     // 로켓프레시 버튼들
     @IBOutlet weak var rocketFreshStackView: UIStackView!
-    
+    // 추천 검색어 버튼들
+    @IBOutlet weak var recommendedSearchWordStackView: UIStackView!
     
     
     
@@ -26,9 +27,7 @@ class SearchViewController: UIViewController {
         
         // 최근검색어 버튼들 속성 지정
         for eachButton in recentSearchStackView.subviews as! [UIButton] {
-            eachButton.layer.borderWidth = 1
-            eachButton.layer.borderColor = UIColor(red:222/255, green:225/255, blue:227/255, alpha: 1).cgColor
-            eachButton.layer.cornerRadius = 20
+            settingButton(button: eachButton)
         }
         
         // 로켓프레시 버튼들 속성 지정
@@ -37,5 +36,20 @@ class SearchViewController: UIViewController {
             eachButton.layer.backgroundColor = UIColor(red:229/255, green:246/255, blue:229/255, alpha: 1).cgColor
             eachButton.setTitleColor(UIColor(red:58/255, green:149/255, blue:58/255, alpha: 1), for: .normal)
         }
+        
+        // 추천 검색어 버튼들 속성 지정
+        for eachStackView in recommendedSearchWordStackView.subviews as! [UIStackView] {
+            for eachButton in eachStackView.subviews as! [UIButton] {
+                
+                
+                settingButton(button: eachButton)
+            }
+        }
+    }
+    
+    func settingButton(button: UIButton) {
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor(red:222/255, green:225/255, blue:227/255, alpha: 1).cgColor
+        button.layer.cornerRadius = 20
     }
 }
