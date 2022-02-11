e1eeb279d7c6498e9fc393e65db4da87d3814e74
DongHee-Sin <voslr0601@naver.com>
2022-02-11 12:32:22 +0900

    <추가> 1. 검색 View 최상단 검색창 2. 최근 검색어 리스트 (가로 스크롤) 3. 로켓프레시 리스트 (가로 스크롤)

---
+++ Autolayout/SearchViewController.swift
@@ -0,0 +1,41 @@
+//
+//  SeachViewController.swift
+//  Autolayout
+//
+//  Created by 신동희 on 2022/02/11.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    @IBOutlet weak var searchBar: UITextField!
+    
+    // 최근검색어 버튼들
+    @IBOutlet weak var recentSearchStackView: UIStackView!
+    // 로켓프레시 버튼들
+    @IBOutlet weak var rocketFreshStackView: UIStackView!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        searchBar.becomeFirstResponder()
+        
+        
+        // 최근검색어 버튼들 속성 지정
+        for eachButton in recentSearchStackView.subviews as! [UIButton] {
+            eachButton.layer.borderWidth = 1
+            eachButton.layer.borderColor = UIColor(red:222/255, green:225/255, blue:227/255, alpha: 1).cgColor
+            eachButton.layer.cornerRadius = 20
+        }
+        
+        // 로켓프레시 버튼들 속성 지정
+        for eachButton in rocketFreshStackView.subviews as! [UIButton] {
+            eachButton.layer.cornerRadius = 20
+            eachButton.layer.backgroundColor = UIColor(red:229/255, green:246/255, blue:229/255, alpha: 1).cgColor
+            eachButton.setTitleColor(UIColor(red:58/255, green:149/255, blue:58/255, alpha: 1), for: .normal)
+        }
+    }
+}
